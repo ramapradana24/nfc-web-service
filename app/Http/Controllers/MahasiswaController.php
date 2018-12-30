a13e21c1b52556e509b6c7d820bfdabe3846c251
Rama Pradana <ramapradana67@hotmail.com>
2018-12-31 01:18:04 +0800

    update

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -24,7 +24,43 @@ class MahasiswaController extends Controller
      */
     public function create()
     {
-        //
+        $validator = Validator::make($r->all(), [
+            "nim" => "required",
+            "nama" => "required",
+            "rfid" => "required",
+            "ortu_nama" => "required",
+            "ortu_username" => "required",
+            "ortu_password" => "required"
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                "status" => false,
+                "pesan" => $validator->errors()->first()
+            ], 500);
+        }
+        $mahasiswa = mahasiswa::where("nim", $r->nim)
+        ->orWhere("rfid", $r->rfid)
+        ->first();
+        if(!$mahasiswa){
+            $ortu = new ortu;
+            $ortu->nama = $r->ortu_nama;
+            $ortu->username = $r->ortu_username;
+            $ortu->password = bcrypt($r->ortu_password);
+            $ortu->save();
+            $mahasiswa = new mahasiswa;
+            $mahasiswa->nama = $r->nama;
+            $mahasiswa->nim = $r->nim;
+            $mahasiswa->rfid = $r->rfid;
+            $mahasiswa->ortu_id = $ortu->id_ortu;
+            $mahasiswa->save();
+            return response()->json([
+                "pesan" => "Mahasiswa berhasil dimasukan"
+            ]);
+        } else {
+            return response()->json([
+                "pesan" => "Mahasiswa dengan NIM atau RFID tersebut sudah ada"
+            ], 500);
+        }
     }
 
     /**
@@ -47,15 +83,17 @@ class MahasiswaController extends Controller
     public function show($rfid)
     {
         $mahasiswa = mahasiswa::where("rfid", $rfid)->first();
-
+ 
         if($mahasiswa){
             return response()->json([
                 "status" => true,
+                "pesan" => "Mahasiswa dengan RFID ".$rfid." ditemukan",
                 "mahasiswa" => $mahasiswa
             ], 200);
         } else {
             return response()->json([
                 "status" => false,
+                "pesan" => "Mahasiswa dengan RFID ".$rfid." tidak ditemukan",
                 "mahasiswa" => null
             ], 500);
         }
